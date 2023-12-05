025c7d83aea0acdd8f7ac7bffd9a9dde4e57fb2b
Shaima Hamila <hamilachaima18@gmail.com>
2023-12-05 02:48:23 +0100

    CRUD backend using event driven rabbitMQ and http axios

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { appDataSource } from "../app-data-source"
 import { Product } from './entity/Product';
 import cors from "cors";
+import * as amqp from 'amqplib/callback_api';
 // establish database connection
 appDataSource
     .initialize()
@@ -17,115 +18,138 @@ appDataSource
 dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8001;
-//This app will run on port 8000 but the frontend aps will have different ports,chrom will prevent this request(coz different ports), so we added cors to make sure that oure frontend will communicate with node
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:8082', 'http://localhost:4200']
-}))
-// Add body parser middleware
-app.use(express.json());
 const productRepository = appDataSource.getRepository(Product)
-//Get all products
-app.get('/api/products', async (req: Request, res: Response) => {
-    const products = await productRepository.find();
-    res.status(200).json({
-        success: true,
-        message: "Fetch all products",
-        data: products
-    })
-});
 
-//Create product
-app.post('/api/products', async (req: Request, res: Response) => {
-    const product = await productRepository.create(req.body)
-    const newProduct = await productRepository.save(product)
-    console.log("newProduct", newProduct)
-
-    res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        data: newProduct
-    })
-})
-
-// Get a single product
-app.get('/api/products/:id', async (req: Request, res: Response) => {
-    const productId = parseInt(req.params.id);
-    const product = await productRepository.findOneBy({ id: productId })
-
-    if (product) {
-        // Product found, send it in the response
-        res.status(200).json({
-            success: true,
-            message: "Product found",
-            data: product
-        });
-    } else {
-        // Product not found
-        res.status(404).json({
-            success: false,
-            message: "Product not found",
-            data: null
-        });
+amqp.connect('amqps://xfsopmhv:mBormtTydv1NCWXIcg1RLCryA_hEdped@octopus.rmq3.cloudamqp.com/xfsopmhv', (error0, connection) => {
+    if (error0) {
+        throw error0
     }
-})
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1
+        }
 
-//Update Product
-app.put('/api/products/:id', async (req: Request, res: Response) => {
-    const productId = parseInt(req.params.id);
-    const product = await productRepository.findOneBy({ id: productId })
-    if (product) {
-        productRepository.merge(product, req.body)
-        const updatedProduct = await productRepository.save(product)
-        res.status(200).json({
-            success: true,
-            message: "Product updated successfully",
-            data: updatedProduct
+        //This app will run on port 8000 but the frontend aps will have different ports,chrom will prevent this request(coz different ports), so we added cors to make sure that oure frontend will communicate with node
+        app.use(cors({
+            origin: ['http://localhost:3000', 'http://localhost:8082', 'http://localhost:4200']
+        }))
+        // Add body parser middleware
+        app.use(express.json());
+        //Get all products
+        app.get('/api/products', async (req: Request, res: Response) => {
+            const products = await productRepository.find();
+            //This is the event we will send it to the admin app
+            res.status(200).json({
+                success: true,
+                message: "Fetch all products",
+                data: products
+            })
         });
-    } else {
-        // Product not found
-        res.status(404).json({
-            success: false,
-            message: "Product not found",
-            data: null
-        });
-    }
-})
 
-//Delete Product
-app.delete('/api/products/:id', async (req: Request, res: Response) => {
-    const deletedProduct = productRepository.delete(req.params.id)
-    res.status(200).json({
-        success: true,
-        message: "Product deleted successfully",
-        data: deletedProduct
+        //Create product
+        app.post('/api/products', async (req: Request, res: Response) => {
+            const product = await productRepository.create(req.body)
+            const newProduct = await productRepository.save(product)
+            console.log("newProduct", newProduct)
+            channel.sendToQueue('product_created', Buffer.from(JSON.stringify(newProduct)))
+            res.status(201).json({
+                success: true,
+                message: "Product created successfully",
+                data: newProduct
+            })
+        })
+
+        // Get a single product
+        app.get('/api/products/:id', async (req: Request, res: Response) => {
+            const productId = parseInt(req.params.id);
+            const product = await productRepository.findOneBy({ id: productId })
+
+            if (product) {
+                // Product found, send it in the response
+                res.status(200).json({
+                    success: true,
+                    message: "Product found",
+                    data: product
+                });
+            } else {
+                // Product not found
+                res.status(404).json({
+                    success: false,
+                    message: "Product not found",
+                    data: null
+                });
+            }
+        })
+
+        //Update Product
+        app.put('/api/products/:id', async (req: Request, res: Response) => {
+            const productId = parseInt(req.params.id);
+            const product = await productRepository.findOneBy({ id: productId })
+            if (product) {
+                productRepository.merge(product, req.body)
+                const updatedProduct = await productRepository.save(product)
+                channel.sendToQueue('product_updated', Buffer.from(JSON.stringify(updatedProduct)))
+                res.status(200).json({
+                    success: true,
+                    message: "Product updated successfully",
+                    data: updatedProduct
+                });
+            } else {
+                // Product not found
+                res.status(404).json({
+                    success: false,
+                    message: "Product not found",
+                    data: null
+                });
+            }
+        })
+
+        //Delete Product
+        app.delete('/api/products/:id', async (req: Request, res: Response) => {
+            const deletedProduct = productRepository.delete(req.params.id)
+            channel.sendToQueue('product_deleted', Buffer.from(req.params.id))
+            res.status(200).json({
+                success: true,
+                message: "Product deleted successfully",
+                data: deletedProduct
+            })
+
+        })
+
+        //update like
+        app.post('/api/products/:id/like', async (req: Request, res: Response) => {
+            const productId = parseInt(req.params.id);
+            const product = await productRepository.findOneBy({ id: productId })
+            if (product) {
+                product.likes++
+                const updatedProduct = await productRepository.save(product)
+                res.status(200).json({
+                    success: true,
+                    message: "Like increased successfully",
+                    data: updatedProduct
+                });
+            } else {
+                // Product not found
+                res.status(404).json({
+                    success: false,
+                    message: "Product not found",
+                    data: null
+                });
+            }
+        })
+
+        app.listen(port, () => {
+            console.log(`Server is Fire at http://localhost:${port}`);
+
+        });
+
+        //We have to stop rebbitMQ connection when the server stops
+        process.on('beforeExit', () => {
+            console.log('clawzed :p')
+            connection.close()
+        })
     })
-
 })
 
-//update like
-app.put('/api/products/:id/like', async (req: Request, res: Response) => {
-    const productId = parseInt(req.params.id);
-    const product = await productRepository.findOneBy({ id: productId })
-    if (product) {
-        product.likes++
-        const updatedProduct = await productRepository.save(product)
-        res.status(200).json({
-            success: true,
-            message: "Like increased successfully",
-            data: updatedProduct
-        });
-    } else {
-        // Product not found
-        res.status(404).json({
-            success: false,
-            message: "Product not found",
-            data: null
-        });
-    }
-})
 
-app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`);
-
-});
 
--- main/src/index.ts
+++ main/src/index.ts
@@ -4,6 +4,9 @@ import dotenv from 'dotenv';
 import { appDataSource } from "../app-data-source"
 import cors from "cors";
 import { Product } from './entity/Products';
+import * as amqp from 'amqplib/callback_api';
+import axios from 'axios'
+import { ObjectId } from 'mongodb'
 // establish database connection
 appDataSource
     .initialize()
@@ -16,39 +19,124 @@ appDataSource
 
 //For env File
 dotenv.config();
-
-const app: Application = express();
-const port = process.env.PORT || 8002;
-//This app will run on port 8000 but the frontend aps will have different ports,chrom will prevent this request(coz different ports), so we added cors to make sure that oure frontend will communicate with node
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:8082', 'http://localhost:4200']
-}))
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Express & TypeScript Server');
-});
 const productRepository = appDataSource.getRepository(Product)
-//Get all products
-app.get('/api/products', async (req: Request, res: Response) => {
-    const products = await productRepository.find();
-    res.status(200).json({
-        success: true,
-        message: "Fetch all products",
-        data: products
-    })
-});
 
-//Create product
-app.post('/api/products', async (req: Request, res: Response) => {
-    const product = await productRepository.create(req.body)
-    const newProduct = await productRepository.save(product)
-    console.log("newProduct", newProduct)
 
-    res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        data: newProduct
+amqp.connect('amqps://xfsopmhv:mBormtTydv1NCWXIcg1RLCryA_hEdped@octopus.rmq3.cloudamqp.com/xfsopmhv', (error0, connection) => {
+    if (error0) {
+        throw error0
+    }
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1
+        }
+        channel.assertQueue('product_created', { durable: false })
+        channel.assertQueue('product_updated', { durable: false })
+        channel.assertQueue('product_deleted', { durable: false })
+
+        const app: Application = express();
+        const port = process.env.PORT || 8002;
+        app.use(express.json());
+        //This app will run on port 8000 but the frontend aps will have different ports,chrom will prevent this request(coz different ports), so we added cors to make sure that oure frontend will communicate with node
+        app.use(cors({
+            origin: ['http://localhost:3000', 'http://localhost:8082', 'http://localhost:4200']
+        }))
+
+        //Create product event
+        channel.consume('product_created', async (msg) => {
+            const eventProduct: Product = JSON.parse(msg!.content.toString())
+            const product = new Product()
+            product.admin_id = parseInt(eventProduct.id)
+            product.title = eventProduct.title
+            product.image = eventProduct.image
+            product.likes = eventProduct.likes
+            await productRepository.save(product)
+            console.log("Product Created")
+        }, { noAck: true })
+
+        //Update product event
+        channel.consume('product_updated', async (msg) => {
+            const eventUpdatedProduct: Product = JSON.parse(msg!.content.toString())
+            const toUpdateproduct = await productRepository.findOneBy({ admin_id: parseInt(eventUpdatedProduct.id) })
+            productRepository.merge(toUpdateproduct!, {
+                title: eventUpdatedProduct.title,
+                image: eventUpdatedProduct.image,
+                likes: eventUpdatedProduct.likes
+            })
+            await productRepository.save(toUpdateproduct!)
+            console.log("Product updated")
+        }, { noAck: true })
+
+        //Delete product
+        channel.consume('product_deleted', async (msg) => {
+            const admin_id = parseInt(msg!.content.toString())
+            await productRepository.delete({ admin_id })
+        })
+
+        //Get all products
+        app.get('/api/products', async (req: Request, res: Response) => {
+            const products = await productRepository.find()
+            res.status(200).json({
+                success: true,
+                message: "Featch all products",
+                data: products
+            })
+        })
+
+        //Increase likes
+        app.post('/api/products/:id/like', async (req: Request, res: Response) => {
+            const productId = parseInt(req.params.id);
+            const product = await productRepository.findOneBy({ admin_id: productId })
+            console.log('Found Product:', product);
+            if (product) {
+                await axios.post(`http://localhost:8001/api/products/${product.admin_id}/like`, {})
+                product!.likes++
+                await productRepository.save(product!)
+                res.status(200).json({
+                    success: true,
+                    message: "Featch all products",
+                    data: product
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "error product not found",
+
+                })
+            }
+
+        })
+
+        // Get a single product
+        app.get('/api/products/:id', async (req: Request, res: Response) => {
+            const productId = parseInt(req.params.id);
+            const product = await productRepository.findOneBy({ admin_id: productId })
+
+            if (product) {
+                // Product found, send it in the response
+                res.status(200).json({
+                    success: true,
+                    message: "Product found",
+                    data: product
+                });
+            } else {
+                // Product not found
+                res.status(404).json({
+                    success: false,
+                    message: "Product not found",
+                    data: null
+                });
+            }
+        })
+
+        app.listen(port, () => {
+            console.log(`Server is Fire at http://localhost:${port}`);
+        });
+        process.on('beforeExit', () => {
+            console.log('clawzed :p')
+            connection.close()
+        })
+
     })
 })
-app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`);
-});
+
